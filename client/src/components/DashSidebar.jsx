f27973bbcbc7d39efbe837a72ffe4a340ba36c60
Aman Ahamed <amanahamed04gmail.com>
2024-11-04 22:42:28 +0530

    complete sidebar

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,40 @@
+import { Sidebar } from "flowbite-react";
+import { FaUser } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSidebar() {
+  const location = useLocation();
+  console.log(location);
+  const [tab, settab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log(tabFromUrl);
+    if (tabFromUrl) {
+      settab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to={"/dashboard?tab=profile"}>
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={FaUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={FaSignOutAlt} classname="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
