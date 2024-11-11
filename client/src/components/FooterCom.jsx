26f3af101f5806136794b7316922aebcd45ec33d
Aman Ahamed <amanahamed04gmail.com>
2024-11-11 21:40:52 +0530

    complete post page functionality

--- client/src/components/FooterCom.jsx
+++ client/src/components/FooterCom.jsx
@@ -7,7 +7,7 @@ import { FaGithub } from "react-icons/fa";
 
 export default function FooterCom() {
   return (
-    <Footer container className="border border-t-8 border-teal-500">
+    <Footer container className="mt-6 border border-t-8 border-teal-500">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
