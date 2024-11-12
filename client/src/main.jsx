5590ed6bde2db70fcb8b62bd48e7638d3e196d87
Aman Ahamed <amanahamed04gmail.com>
2024-11-12 13:39:39 +0530

    add cooment route api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,11 @@ import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import ThemeProvider from "./components/ThemeProvider.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+  <ScrollToTop/>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
         <ThemeProvider>
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 export default function PostPage() {
   const params = useParams();
@@ -38,11 +39,11 @@ export default function PostPage() {
   return (
     <div>
       {loading ? (
-        <div className="flex justify-center items-center max-h-screen">
+        <div className="flex justify-center items-center min-h-screen">
           <Spinner size="xl" />
         </div>
       ) : (
-        <main className="p-3 flex flex-col max-w-6xl mx-auto max-h-screen">
+        <div className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
           <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
             {post && post.title}
           </h1>
@@ -59,7 +60,7 @@ export default function PostPage() {
             alt={post && post.title}
             className="mt-10 p-3 max-h-[600px] w-full object-cover"
           />
-          <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
+          <div className="flex justify-between gap-4 p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
             <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
             <span className="italic">
               {post && (post.content.length / 1000).toFixed(0)} mins read
@@ -72,7 +73,8 @@ export default function PostPage() {
           <div className="max-w-4xl mx-auto w-full">
             <CallToAction />
           </div>
-        </main>
+          <CommentSection postId={post._id} />
+        </div>
       )}
     </div>
   );
