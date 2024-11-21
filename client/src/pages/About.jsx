abbd8c25c8484e572b4b263ab77fc4cc07926014
Aman Ahamed <amanahamed04gmail.com>
2024-11-21 12:57:27 +0530

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,39 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Aman's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
+              hic officiis incidunt asperiores. Molestiae voluptate voluptatem
+              fugit incidunt odit dicta ipsa. Repellendus eaque quia aliquam
+              quaerat maiores labore aut cupiditate quasi sint rerum. Porro
+              magni natus eos impedit et, quasi unde ullam? Perspiciatis,
+              dolorem deleniti!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
+              hic officiis incidunt asperiores. Molestiae voluptate voluptatem
+              fugit incidunt odit dicta ipsa. Repellendus eaque quia aliquam
+              quaerat maiores labore aut cupiditate quasi sint rerum. Porro
+              magni natus eos impedit et, quasi unde ullam? Perspiciatis,
+              dolorem deleniti!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
+              hic officiis incidunt asperiores. Molestiae voluptate voluptatem
+              fugit incidunt odit dicta ipsa. Repellendus eaque quia aliquam
+              quaerat maiores labore aut cupiditate quasi sint rerum. Porro
+              magni natus eos impedit et, quasi unde ullam? Perspiciatis,
+              dolorem deleniti!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
